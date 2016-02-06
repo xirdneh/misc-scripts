02e287ba351e7ad89345b315750979bdd8813fa4
Josue Balandrano Coronel <xirdneh@gmail.com>
2016-02-05 23:15:54 -0600

    Deleting list of metas

--- tacc/agave/agave_file_walker/main.py
+++ tacc/agave/agave_file_walker/main.py
@@ -1,12 +1,31 @@
 from agavepy.agave import Agave
 import sys
 
+import imp
+afm = imp.load_source('AgaveFileManager', '/Users/xirdneh/projects/django/designsafe/portal/dsapi/agave/daos.py')
+CURSOR_UP_ONE = '\x1b[1A'
+ERASE_LINE = '\x1b[2K'
+
+
+def walk(c, system_id, folder, tab):
+    files = c.files.list(systemId = system_id, filePath = folder)
+    for f in files:
+        if f['name'] == '.' or f['name'] == '..':
+            continue
+        af = afm.AgaveFolderFile(agave_client = c, file_obj = f)
+        print '|' + '-' * tab + af.name
+        print 'AgaveFile: {}'.format(af.as_json())
+        m = af.save_as_metadata()
+        if f['format'] == 'folder':
+            walk(c, system_id, f['path'], tab + 1)
+
 if __name__ == '__main__':
     if len(sys.argv) < 4:
-        print 'Usage <api_server> <token> <base_folder>'
+        print 'Usage <api_server> <token> <systemId> <base_folder>'
     args = sys.argv[1:]
     url = args[0]
     token = args[1]
-    base_folder = args[2]
+    system_id = args[2]
+    base_folder = args[3]
     c = Agave(api_server = url, token = token)
-
+    walk(c, system_id, base_folder, 0)   
--- tacc/agave/meta_batch_del.sh
+++ tacc/agave/meta_batch_del.sh
@@ -1,4 +1,4 @@
-metadata-list -Q '{"name": "designsafe metadata"}' | grep '"uuid":' | awk '{print $2}' | sed -e 's/[\",]//g' | while read x; do
+metadata-list -Q '{"name": "object"}' | grep '"uuid":' | awk '{print $2}' | sed -e 's/[\",]//g' | while read x; do
     metadata-delete $x
     echo $x
 done
